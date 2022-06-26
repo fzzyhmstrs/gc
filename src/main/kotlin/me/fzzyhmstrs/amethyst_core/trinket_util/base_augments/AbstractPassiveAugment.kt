16c6ed0c82b1b6fdfd24545035ce3baef9b51b5b
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-06-26 19:42:45 -0400

    Tweaking base augments

--- src/main/kotlin/me/fzzyhmstrs/amethyst_core/trinket_util/base_augments/AbstractPassiveAugment.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_core/trinket_util/base_augments/AbstractPassiveAugment.kt
@@ -8,8 +8,11 @@ import net.minecraft.item.ItemStack
 
 open class AbstractPassiveAugment(weight: Rarity, mxLvl: Int = 1, vararg slot: EquipmentSlot): BaseAugment(weight,mxLvl,EnchantmentTarget.ARMOR, *slot) {
 
+    override fun equipEffect(user: LivingEntity, level: Int, stack: ItemStack) {
+        tickEffect(user, level, stack)
+    }
+
     open fun tickEffect(user: LivingEntity, level: Int, stack: ItemStack = ItemStack.EMPTY){
         return
     }
-
 }
