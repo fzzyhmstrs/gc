30eed9fc2a6dd1f09693aae7d7f5db7729aeeaa8
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-06-27 08:35:50 -0400

    Updating dustbin and adding TickingDustbin
    simplifying the dustbin execution to list-based, rather than push-last-out based. TickingDustbin simply cleans itself every tick.

--- src/main/kotlin/me/fzzyhmstrs/amethyst_core/coding_util/Dustbin.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_core/coding_util/Dustbin.kt
@@ -1,25 +1,36 @@
 package me.fzzyhmstrs.amethyst_core.coding_util
 
 import java.util.function.Consumer
+import me.fzzyhmstrs.amethyst_core.registry.*
 
-data class Dustbin<T: Comparable<T>>(private val consumer: Consumer<T>, private val noDust: T, private var dust: T = noDust){
-    private var dirty: Boolean = false
+open class Dustbin<T>(private val consumer: Consumer<T>){
+    protected var dirty: Boolean = false
+    protected val dust: MutableList<T> = mutableListOf()
     fun markDirty(newDust: T){
-        if (isDirty()) {
-            consumer.accept(dust)
-        }
-        dust = newDust
-        checkDirty()
+        dust.add(newDust)
+        dirty = true
     }
     fun isDirty(): Boolean{
         return dirty
     }
-    private fun checkDirty(){
-        dirty = dust != noDust
-    }
     fun clean(){
-        consumer.accept(dust)
-        dust = noDust
-        checkDirty()
+        if (!dirty) return
+        dust.forEach{
+            consumer.accept(it)
+        }
+        dust.clear()
+        dirty = false
     }
+}
+
+open class TickingDustbin<T>(consumer: Consumer<T>): Dustbin(consumer), TickUppable{
+    
+    init{
+        EventRegistry.registerTickUppable(this)
+    }
+    
+    override fun tickUp(){
+        clean()
+    }    
+    
 }
