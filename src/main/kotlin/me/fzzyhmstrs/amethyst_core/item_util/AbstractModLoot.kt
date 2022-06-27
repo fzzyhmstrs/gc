f4e99f4b3fd230c33a4a5aa91cc21d86ac0cdd2b
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-06-26 20:12:26 -0400

    adding abstract targetNameSpace to the loot abstract class. moving the namespace check to an internal fun

--- src/main/kotlin/me/fzzyhmstrs/amethyst_core/item_util/AbstractModLoot.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_core/item_util/AbstractModLoot.kt
@@ -3,8 +3,15 @@ package me.fzzyhmstrs.amethyst_core.item_util
 import net.fabricmc.fabric.api.loot.v1.FabricLootSupplierBuilder
 import net.minecraft.util.Identifier
 
-interface AbstractModLoot {
+abstract class AbstractModLoot {
 
-    fun lootBuilder(id: Identifier, table: FabricLootSupplierBuilder): Boolean
+    abstract val targetNameSpace: String
+
+    abstract fun lootBuilder(id: Identifier, table: FabricLootSupplierBuilder): Boolean
+
+    internal fun buildLoot(id: Identifier, table: FabricLootSupplierBuilder): Boolean{
+        if (id.namespace != targetNameSpace) return false
+        return lootBuilder(id, table)
+    }
 
 }
