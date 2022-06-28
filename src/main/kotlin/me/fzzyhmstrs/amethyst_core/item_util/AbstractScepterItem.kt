a10b52d499d195f2eb4b7b9558234eb29101f87f
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-06-27 21:12:19 -0400

    BIg work on getting a scepter framework in place

--- src/main/kotlin/me/fzzyhmstrs/amethyst_core/item_util/AbstractScepterItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_core/item_util/AbstractScepterItem.kt
@@ -1,46 +1,31 @@
 package me.fzzyhmstrs.amethyst_core.item_util
 
-import me.fzzyhmstrs.amethyst_core.AC
-import me.fzzyhmstrs.amethyst_core.coding_util.PlayerParticles.scepterParticlePos
-import me.fzzyhmstrs.amethyst_core.nbt_util.Nbt
-import me.fzzyhmstrs.amethyst_core.nbt_util.NbtKeys
+import me.fzzyhmstrs.amethyst_core.mana_util.ManaItem
 import me.fzzyhmstrs.amethyst_core.scepter_util.ScepterHelper
 import me.fzzyhmstrs.amethyst_core.scepter_util.ScepterToolMaterial
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
-import net.fabricmc.fabric.api.networking.v1.PacketByteBufs
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
-import net.minecraft.client.MinecraftClient
-import net.minecraft.enchantment.EnchantmentHelper
-import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.Entity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
-import net.minecraft.item.ToolItem
 import net.minecraft.nbt.NbtCompound
-import net.minecraft.nbt.NbtList
-import net.minecraft.particle.ParticleTypes
-import net.minecraft.server.network.ServerPlayerEntity
-import net.minecraft.text.LiteralText
-import net.minecraft.text.MutableText
-import net.minecraft.util.Formatting
-import net.minecraft.util.Identifier
-import net.minecraft.util.UseAction
+import net.minecraft.util.*
 import net.minecraft.util.math.MathHelper
-import net.minecraft.util.registry.Registry
 import net.minecraft.world.World
 
-abstract class AbstractScepterItem(material: ScepterToolMaterial, settings: Settings, vararg defaultModifier: Identifier): ToolItem(material, settings), ManaItem {
+abstract class AbstractScepterItem(material: ScepterToolMaterial, settings: Settings): CustomFlavorToolItem(material, settings), ManaItem {
 
-    private val tickerManaRepair: Long
-    protected val defaultModifiers: MutableList<Identifier> = mutableListOf()
+    private val tickerManaRepair: Long = material.healCooldown()
     abstract val fallbackId: Identifier
 
-    init {
-        tickerManaRepair = material.healCooldown()
-        defaultModifier.forEach {
-            defaultModifiers.add(it)
+    override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
+        val stack = user.getStackInHand(hand)
+        val nbt = stack.orCreateNbt
+        if (needsInitialization(stack, nbt)){
+            initializeScepter(stack, nbt)
         }
+        return super.use(world, user, hand)
     }
 
-    fun getRepairTime(): Int{
+    override fun getRepairTime(): Int{
         return tickerManaRepair.toInt()
     }
 
@@ -56,51 +41,29 @@ abstract class AbstractScepterItem(material: ScepterToolMaterial, settings: Sett
         return UseAction.BLOCK
     }
 
-    open fun defaultActiveEnchant(): Identifier{
-        return fallbackId
+    override fun onCraft(stack: ItemStack, world: World, player: PlayerEntity) {
+        val nbt = stack.orCreateNbt
+        writeDefaultNbt(stack, nbt)
+        initializeScepter(stack, nbt)
+    }
+
+    open fun writeDefaultNbt(stack: ItemStack, scepterNbt: NbtCompound){
+    }
+
+    open fun initializeScepter(stack: ItemStack, scepterNbt: NbtCompound){
+        ScepterHelper.initializeManaItem(stack)
+    }
+
+    open fun needsInitialization(stack: ItemStack, scepterNbt: NbtCompound): Boolean{
+        return false
     }
     
     //removes cooldown on the item if you switch item
     override fun inventoryTick(stack: ItemStack, world: World, entity: Entity, slot: Int, selected: Boolean) {
         if (world.isClient) return
-        if (entity !is PlayerEntity) return
         //slowly heal damage over time
         if (ScepterHelper.tickTicker(stack)){
             healDamage(1,stack)
         }
     }
-
-    companion object{
-       
-        fun writeDefaultNbt(stack: ItemStack){
-            val nbt = stack.orCreateNbt
-            val item = stack.item
-            if (item is AbstractScepterItem) {
-                if(!nbt.contains(NbtKeys.ACTIVE_ENCHANT.str())){
-                    val identifier = item.fallbackId
-                    Nbt.writeStringNbt(NbtKeys.ACTIVE_ENCHANT.str(), identifier.toString(), nbt)
-                }
-                val nbtList = NbtList()
-                item.defaultModifiers.forEach {
-                    val nbtEl = NbtCompound()
-                    Nbt.writeStringNbt(NbtKeys.MODIFIER_ID.str(),it.toString(), nbtEl)
-                    nbtList.add(nbtEl)
-                }
-                nbt.put(NbtKeys.MODIFIERS.str(),nbtList)
-            }
-            ScepterHelper.getScepterStats(stack)
-        }
-        fun addDefaultEnchantment(stack: ItemStack){
-            val item = stack.item
-            if (item is AbstractScepterItem) {
-                val enchantToAdd = Registry.ENCHANTMENT.get(item.fallbackId)
-                if (enchantToAdd != null){
-                    if (EnchantmentHelper.getLevel(enchantToAdd,stack) == 0){
-                        stack.addEnchantment(enchantToAdd,1)
-                    }
-                }
-            }
-        }
-    }
-
 }
---
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_core/item_util/ModifiableScepterItem.kt
@@ -0,0 +1,56 @@
+package me.fzzyhmstrs.amethyst_core.item_util
+
+import me.fzzyhmstrs.amethyst_core.item_util.interfaces.Modifiable
+import me.fzzyhmstrs.amethyst_core.modifier_util.AugmentModifier
+import me.fzzyhmstrs.amethyst_core.modifier_util.ModifierHelper
+import me.fzzyhmstrs.amethyst_core.nbt_util.Nbt
+import me.fzzyhmstrs.amethyst_core.nbt_util.NbtKeys
+import me.fzzyhmstrs.amethyst_core.scepter_util.ScepterToolMaterial
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.nbt.NbtList
+import net.minecraft.util.Identifier
+
+abstract class ModifiableScepterItem(material: ScepterToolMaterial, settings: Settings): AbstractScepterItem(material, settings), Modifiable{
+
+    override val defaultModifiers: MutableList<Identifier> = mutableListOf()
+
+    fun withModifiers(defaultMods: List<AugmentModifier> = listOf()): ModifiableScepterItem{
+        defaultMods.forEach {
+            defaultModifiers.add(it.modifierId)
+        }
+        return this
+    }
+
+    override fun writeDefaultNbt(stack: ItemStack, scepterNbt: NbtCompound){
+        super.writeDefaultNbt(stack, scepterNbt)
+        if (!scepterNbt.contains(NbtKeys.MODIFIERS.str())) {
+            val nbtList = NbtList()
+            defaultModifiers.forEach {
+                val nbtEl = NbtCompound()
+                Nbt.writeStringNbt(NbtKeys.MODIFIER_ID.str(), it.toString(), nbtEl)
+                nbtList.add(nbtEl)
+            }
+            scepterNbt.put(NbtKeys.MODIFIERS.str(), nbtList)
+        }
+    }
+
+    override fun initializeScepter(stack: ItemStack, scepterNbt: NbtCompound) {
+        super.initializeScepter(stack, scepterNbt)
+        if (modifiersNeedInit(scepterNbt)){
+            writeDefaultNbt(stack, scepterNbt)
+        }
+        ModifierHelper.initializeModifiers(stack, scepterNbt)
+    }
+
+    override fun needsInitialization(stack: ItemStack, scepterNbt: NbtCompound): Boolean {
+        return super.needsInitialization(stack, scepterNbt) || modifiersNeedInit(scepterNbt)
+    }
+
+    private fun modifiersNeedInit(scepterNbt: NbtCompound): Boolean{
+        return defaultModifiers.isNotEmpty() && !scepterNbt.contains(NbtKeys.MODIFIERS.str())
+    }
+
+
+
+}
