b9bb238aca3da09c1ce8ea4003b6f31d39edca92
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-07-13 19:41:11 -0400

    Fixing translatable text call in default scepter tooltip
    (cherry picked from commit 5b2605f1086e726fc4ee4c0267c8de61ae81b31a)

--- src/main/kotlin/me/fzzyhmstrs/amethyst_core/item_util/DefaultScepterItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_core/item_util/DefaultScepterItem.kt
@@ -58,9 +58,9 @@ abstract class DefaultScepterItem(material: ScepterToolMaterial, settings: Setti
         val nbt = stack.orCreateNbt
         val activeSpell = if (nbt.contains(NbtKeys.ACTIVE_ENCHANT.str())) {
             val activeEnchantId = Nbt.readStringNbt(NbtKeys.ACTIVE_ENCHANT.str(), nbt)
-            TranslatableText("enchantment.amethyst_imbuement.${Identifier(activeEnchantId).path}")
+            TranslatableText("enchantment.${Identifier(activeEnchantId).namespace}.${Identifier(activeEnchantId).path}")
         } else {
-            TranslatableText("enchantment.amethyst_imbuement.none")
+            TranslatableText("enchantment.amethyst_core.none")
         }
         tooltip.add(TranslatableText("scepter.active_spell").formatted(Formatting.GOLD).append(activeSpell.formatted(Formatting.GOLD)))
         val stats = ScepterHelper.getScepterStats(stack)
