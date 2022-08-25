6cd5915c6af50357bc40fc67e7b1bff1ac1dea5d
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-08-24 21:58:25 -0400

    updated PlayerCreatable.kt with a tick check and isCreated check
    (cherry picked from commit c868604cff60513b37989a4d8ab7663dc71f4004)

--- src/main/kotlin/me/fzzyhmstrs/amethyst_core/entity_util/PlayerCreatable.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_core/entity_util/PlayerCreatable.kt
@@ -1,6 +1,7 @@
 package me.fzzyhmstrs.amethyst_core.entity_util
 
 import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.mob.Angerable
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.world.World
@@ -11,6 +12,10 @@ interface PlayerCreatable {
     var maxAge: Int
     var createdBy: UUID?
     var owner: LivingEntity?
+
+    fun isPlayerCreated(): Boolean {
+        return (maxAge > 0) || createdBy != null
+    }
 
     fun writePlayerCreatedNbt(nbt: NbtCompound){
         nbt.putInt("MaxAge", maxAge)
@@ -29,6 +34,17 @@ interface PlayerCreatable {
                 if (chkEntity is LivingEntity) {
                     owner = chkEntity
                 }
+            }
+        }
+    }
+
+    fun tickOwnerAttacked(entity: Angerable){
+        if (owner != null){
+            val attacker = owner?.recentDamageSource?.attacker
+            if (attacker != null && attacker is LivingEntity){
+                entity.target = attacker
+                entity.angryAt = attacker.uuid
+                entity.chooseRandomAngerTime()
             }
         }
     }
