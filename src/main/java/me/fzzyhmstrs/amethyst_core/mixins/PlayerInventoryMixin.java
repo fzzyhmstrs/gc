ac339142e766f231bb203ab2059620dfab09c5fc
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-08-28 20:06:10 -0400

    updated spell-switching code so it doesn't rely on posing.
    (cherry picked from commit 74c60de41aeb05838faf15ee5be6cecff8b61935)

--- src/main/java/me/fzzyhmstrs/amethyst_core/mixins/PlayerInventoryMixin.java
+++ src/main/java/me/fzzyhmstrs/amethyst_core/mixins/PlayerInventoryMixin.java
@@ -2,7 +2,7 @@ package me.fzzyhmstrs.amethyst_core.mixins;
 
 import me.fzzyhmstrs.amethyst_core.item_util.AbstractScepterItem;
 import me.fzzyhmstrs.amethyst_core.scepter_util.ScepterHelper;
-import net.minecraft.entity.EntityPose;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.Hand;
@@ -24,8 +24,9 @@ public abstract class PlayerInventoryMixin {
     @Inject(at = @At("HEAD"), method = "scrollInHotbar", cancellable = true)
     private void scrollInHotbar(double scrollAmount, CallbackInfo ci) {
         //System.out.println(player.getStackInHand(Hand.MAIN_HAND).getItem().toString());
-        if (player.getStackInHand(Hand.MAIN_HAND).getItem() instanceof AbstractScepterItem){
-            if (player.getPose().equals(EntityPose.CROUCHING)){
+        if (player.getStackInHand(Hand.MAIN_HAND).getItem() instanceof AbstractScepterItem && player.world.isClient){
+            ClientPlayerEntity entity = (ClientPlayerEntity) player;
+            if (entity.input.sneaking){
                 ScepterHelper.INSTANCE.sendScepterUpdateFromClient(scrollAmount < 0.0D);
                 ci.cancel();
             }
