89fc308035aaef982811526cec9dfbb972d6d260
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-01-18 14:56:04 -0500

    Update EquipmentModifierHelper.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_core/modifier_util/EquipmentModifierHelper.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_core/modifier_util/EquipmentModifierHelper.kt
@@ -10,6 +10,7 @@ object EquipmentModifierHelper: AbstractModifierHelper<EquipmentModifier>() {
     private val targetMap: MultiMap<EquipmentModifierTarget, EquipmentModifier> = ArrayListMultiMap.create()
     private val DEFAULT_MODIFIER_TOLL = BinomialLootNumberProvider.create(5,0.5f)
     private val BLANK_WEAPON_MOD = EquipmentModifier(ModifierDefaults.BLANK_ID)
+    private val tooltips: Map<Long, List<Text>> = mutableMapOf()
     
     override val fallbackData: AbstractModifier<EquipmentModifier>.CompiledModifiers
         get() = BLANK_WEAPON_MOD.CompiledModifiers(listOf(), EquipmentModifier(ModifierDefaults.BLANK_ID))
@@ -18,19 +19,36 @@ object EquipmentModifierHelper: AbstractModifierHelper<EquipmentModifier>() {
         val nbt = stack.nbt
         if (nbt != null) {
             val id = Nbt.getItemStackId(nbt)
-            setModifiersById(
-                id,
-                gatherActiveAbstractModifiers(
+            val compiled = gatherActiveAbstractModifiers(
                     stack,
                     ModifierDefaults.BLANK_ID,
                     BLANK_WEAPON_MOD.compiler()
                 )
+            val list: MutableList<Text> = mutableListOf()
+            compiled.modifiers.forEach{
+                list.add(AcText.translatable(it.getTranslationKey()).formatted(it.rarity.formatting)
+                            .append(AcText.literal(" - ").formatted(it.rarity.formatting)
+                            .append(AcText.translatable(it.getDescTranslationKey()).formatted(it.rarity.formatting).formatted(Formatting.ITALIC)
+                        )
+            }
+            tooltips[id] = list
+            setModifiersById(
+                id,
+                compiled
             )
         }
     }
 
     override fun getTranslationKeyFromIdentifier(id: Identifier): String {
         return "equipment.modifier.${id}"
+    }
+    
+    override fun getDescTranslationKeyFromIdentifier(id: Identifier): String {
+        return "equipment.modifier.${id}.desc"
+    }
+    
+    override fun addModifierTooltip(stack: ItemStack, tooltip: MutableList<Text>){
+        tooltip.addAll(tooltips)
     }
     
     fun addUniqueModifier(modifier: Identifier, stack: ItemStack){
