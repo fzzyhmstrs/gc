28a890fd618dc24d73409d9ab32cf599b5f336f2
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-01-18 12:37:55 -0500

    Update EquipmentModifier.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_core/modifier_util/EquipmentModifier.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_core/modifier_util/EquipmentModifier.kt
@@ -14,22 +14,32 @@ import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 import java.util.*
 
-class EquipmentModifier(modifierId: Identifier = ModifierDefaults.BLANK_ID, val persistent: Boolean = false, val randomSelectable: Boolean = false): AbstractModifier<EquipmentModifier>(modifierId) {
-
-    private val attributeModifiers: Multimap<EntityAttribute, EntityAttributeModifier> = HashMultimap.create()
+class EquipmentModifier(
+    modifierId: Identifier = ModifierDefaults.BLANK_ID, 
+    val target: EquimentModifierTarget = EquipmentModifierTarget.ANY,
+    val weight: Int = 1,
+    val persistent: Boolean = false, 
+    val randomSelectable: Boolean = false): AbstractModifier<EquipmentModifier>(modifierId) {
+    
+    init{
+        EquipmentModifierHelper.addToTargetMap(this)
+    }
+    
+    private val attributeModifiers: Multimap<EntityAttribute, EntityAttributeModifier> = ArrayListMultimap.create()
     private val postHitConsumers: MutableList<ToolConsumer> = mutableListOf()
     private val postMineConsumers: MutableList<MiningConsumer> = mutableListOf()
     private val onUseConsumers: MutableList<ToolConsumer> = mutableListOf()
-    private val onDamagedConsumers: MutableList<DamageConsumer> = mutableListOf()
+    private val onDamagedFunctions: MutableList<DamageFunction> = mutableListOf()
     private val killOtherConsumers: MutableList<ToolConsumer> = mutableListOf()
     private val tickConsumers: MutableList<ToolConsumer> = mutableListOf()
+    private var toll: LootNumberProvider = ConstantLootNumberProvider(5f)
 
     override fun plus(other: EquipmentModifier): EquipmentModifier {
         attributeModifiers.putAll(other.attributeModifiers)
         postHitConsumers.addAll(other.postHitConsumers)
         postMineConsumers.addAll(other.postMineConsumers)
         onUseConsumers.addAll(other.onUseConsumers)
-        onDamagedConsumers.addAll(other.onDamagedConsumers)
+        onDamagedFunctions.addAll(other.onDamagedFunctions)
         killOtherConsumers.addAll(other.killOtherConsumers)
         tickConsumers.addAll(other.tickConsumers)
         return this
@@ -78,15 +88,17 @@ class EquipmentModifier(modifierId: Identifier = ModifierDefaults.BLANK_ID, val 
         }
     }
 
-    fun withOnDamaged(onDamaged: DamageConsumer): EquipmentModifier {
-        onDamagedConsumers.add(onDamaged)
+    fun withOnDamaged(onDamaged: DamageFunction): EquipmentModifier {
+        onDamagedFunctions.add(onDamaged)
         return this
     }
 
-    fun onDamaged(stack: ItemStack, user: LivingEntity, attacker: LivingEntity?, source: DamageSource, amount: Float){
-        onDamagedConsumers.forEach {
-            it.apply(stack, user, attacker, source, amount)
+    fun onDamaged(stack: ItemStack, user: LivingEntity, attacker: LivingEntity?, source: DamageSource, amount: Float): Float{
+        var newAmount = amount
+        onDamagedFunctions.forEach {
+            newAmount = it.test(stack, user, attacker, source, newAmount)
         }
+        return newAmount
     }
 
     fun withKilledOther(killOther: ToolConsumer): EquipmentModifier {
@@ -115,6 +127,11 @@ class EquipmentModifier(modifierId: Identifier = ModifierDefaults.BLANK_ID, val 
         addDescendant(modifier)
         return this
     }
+    
+    fun withToll(toll: LootNumberProvider): EquipmentModifier{
+        this.toll = toll
+        return this
+    }
 
     override fun compiler(): Compiler {
         return Compiler(mutableListOf(), EquipmentModifier())
@@ -135,8 +152,94 @@ class EquipmentModifier(modifierId: Identifier = ModifierDefaults.BLANK_ID, val 
     }
 
     @FunctionalInterface
-    fun interface DamageConsumer{
-        fun apply(stack: ItemStack, user: LivingEntity, attacker: LivingEntity?, source: DamageSource, amount: Float)
+    fun interface DamageFunction{
+        fun test(stack: ItemStack, user: LivingEntity, attacker: LivingEntity?, source: DamageSource, amount: Float): Float
     }
-
+    
+    abstract class EquipmentModifierTarget(val id: Identifier){
+    
+        companion object{
+            
+            internal val targets: MutableList<EquipmentModifierTarget> = mutableListOf()
+            
+            internal fun findTargetForItem(stack: ItemStack): EquipmentModifierTarget?{
+                for (target: targets){
+                    if (target.isAcceptableItem(stack)){
+                        return target
+                    }
+                }
+                return null
+            }
+            
+            val ANY = object: EquipmentModifierTarget(Identifier(Viscerae.MOD_ID,"any")){
+                override fun isAcceptableItem(stack: ItemStack): Boolean{
+                    return stack.isDamagable()
+                }
+            }
+            val SWORD = object: EquipmentModifierTarget(Identifier(Viscerae.MOD_ID,"sword")){
+                override fun isAcceptableItem(stack: ItemStack): Boolean{
+                    return stack.item is SwordItem
+                }
+            }
+            val AXE = object: EquipmentModifierTarget(Identifier(Viscerae.MOD_ID,"axe")){
+                override fun isAcceptableItem(stack: ItemStack): Boolean{
+                    return stack.item is AxeItem
+                }
+            }
+            val TRIDENT = object: EquipmentModifierTarget(Identifier(Viscerae.MOD_ID,"trident")){
+                override fun isAcceptableItem(stack: ItemStack): Boolean{
+                    return stack.item is TridentItem
+                }
+            }
+            val BOW = object: EquipmentModifierTarget(Identifier(Viscerae.MOD_ID,"bow")){
+                override fun isAcceptableItem(stack: ItemStack): Boolean{
+                    return (stack.item is BowItem || stack.item is CrossbowItem)
+                }
+            }
+            val MINING = object: EquipmentModifierTarget(Identifier(Viscerae.MOD_ID,"mining")){
+                override fun isAcceptableItem(stack: ItemStack): Boolean{
+                    return stack.item is MiningToolItem
+                }
+            }
+            val SHIELD = object: EquipmentModifierTarget(Identifier(Viscerae.MOD_ID,"shield")){
+                override fun isAcceptableItem(stack: ItemStack): Boolean{
+                    return stack.item is ShieldItem
+                }
+            }
+            val TOOL = object: EquipmentModifierTarget(Identifier(Viscerae.MOD_ID,"tool")){
+                override fun isAcceptableItem(stack: ItemStack): Boolean{
+                    return stack.item is ToolItem
+                }
+            }
+            val ARMOR = object: EquipmentModifierTarget(Identifier(Viscerae.MOD_ID,"armor")){
+                override fun isAcceptableItem(stack: ItemStack): Boolean{
+                    return stack.item is ArmorItem
+                }
+            }
+            val TRINKET = object: EquipmentModifierTarget(Identifier(Viscerae.MOD_ID,"trinket")){
+                override fun isAcceptableItem(stack: ItemStack): Boolean{
+                    return stack.item is TrinketItem
+                }
+            }
+        }
+        
+        init{
+            if (targets.containsKey(id) throw IllegalStateException("Equipment Modifier target ${} already instantiated!")
+            targets[id] = this
+        }
+        
+        override fun equals(other: Any?): Boolean{
+            if (other == null) return false
+            if (other !is EquipmentModifierTarget) return false
+            return other.id == id
+        }
+        
+        override fun hashcode(): Int{
+            return id.hashcode() + 31 * id.hashcode()
+        }
+        
+        fun isAcceptableItem(stack: ItemStack): Boolean
+        
+    }
+    
 }
