556429633a2cd56db2fbb1c12a617b99a2fcb397
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-02-07 10:50:05 -0500

    Update HitTracking.java

--- src/main/java/me/fzzyhmstrs/gear_core/interfaces/HitTracking.java
+++ src/main/java/me/fzzyhmstrs/gear_core/interfaces/HitTracking.java
@@ -5,6 +5,7 @@ import net.minecraft.item.ItemStack;
 
 public interface HitTracking {
     default void postWearerHit(ItemStack stack, LivingEntity wearer, LivingEntity target){
-
+        AbstractModifier.CompiledModifiers<EquipmentModifier> modifiers = EquipmentModifierHelper.INSTANCE.getActiveModifiers(stack);
+        modifiers.getCompiledData().postHit(stack,wearer,target);
     }
 }
