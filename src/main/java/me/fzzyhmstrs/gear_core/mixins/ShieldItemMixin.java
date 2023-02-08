7362c9037c904b0c446fe51ee8fc724dc66a86f1
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-02-08 13:25:18 -0500

    Create ShieldItemMixin.java

---
+++ src/main/java/me/fzzyhmstrs/gear_core/mixins/ShieldItemMixin.java
@@ -0,0 +1,52 @@
+package me.fzzyhmstrs.gear_core.mixins;
+
+import me.fzzyhmstrs.fzzy_core.interfaces.Modifiable;
+import me.fzzyhmstrs.fzzy_core.modifier_util.AbstractModifier;
+import me.fzzyhmstrs.fzzy_core.modifier_util.ModifierInitializer;
+import me.fzzyhmstrs.gear_core.interfaces.*;
+import me.fzzyhmstrs.gear_core.modifier_util.*;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Mixin;
+
+import java.util.Collections;
+import java.util.List;
+
+@Mixin(ShieldItem.class)
+public class ShieldItemMixin implements HitTracking, KillTracking, DamageTracking, AttributeTracking, Modifiable {
+    
+    @Override
+    public boolean correctSlot(EquipmentSlot slot){
+        return getSlotType() == EquipmentSlot.OFFHAND
+    }
+
+    @NotNull
+    @Override
+    public List<Identifier> defaultModifiers() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void addModifierTooltip(ItemStack stack, List<Text> tooltip, TooltipContext context) {
+        EquipmentModifierHelper.INSTANCE.addModifierTooltip(stack, tooltip, context);
+    }
+
+    @Override
+    public ModifierInitializer getModifierInitializer() {
+        return EquipmentModifierHelper.INSTANCE;
+    }
+}
