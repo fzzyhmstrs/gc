f0fff9fe474482c474541047fd2e886e468f5992
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-02-10 15:57:55 -0500

    optimized imports

--- src/main/java/me/fzzyhmstrs/gear_core/mixins/ShieldItemMixin.java
+++ src/main/java/me/fzzyhmstrs/gear_core/mixins/ShieldItemMixin.java
@@ -1,27 +1,19 @@
 package me.fzzyhmstrs.gear_core.mixins;
 
 import me.fzzyhmstrs.fzzy_core.interfaces.Modifiable;
-import me.fzzyhmstrs.fzzy_core.modifier_util.AbstractModifier;
 import me.fzzyhmstrs.fzzy_core.modifier_util.ModifierInitializer;
-import me.fzzyhmstrs.gear_core.interfaces.*;
-import me.fzzyhmstrs.gear_core.modifier_util.*;
-import net.minecraft.block.BlockState;
+import me.fzzyhmstrs.gear_core.interfaces.AttributeTracking;
+import me.fzzyhmstrs.gear_core.interfaces.DamageTracking;
+import me.fzzyhmstrs.gear_core.interfaces.HitTracking;
+import me.fzzyhmstrs.gear_core.interfaces.KillTracking;
+import me.fzzyhmstrs.gear_core.modifier_util.EquipmentModifierHelper;
 import net.minecraft.client.item.TooltipContext;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ShieldItem;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 
 import java.util.Collections;
