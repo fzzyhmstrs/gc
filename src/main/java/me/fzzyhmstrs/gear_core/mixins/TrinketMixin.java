7fb1a26b5d8951198424ae1b537377db8870f146
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-02-10 12:43:36 -0500

    fix for build
    (cherry picked from commit 9075f993ad828c926940cd899cc5953f19c21c5e)

--- src/main/java/me/fzzyhmstrs/gear_core/mixins/TrinketMixin.java
+++ src/main/java/me/fzzyhmstrs/gear_core/mixins/TrinketMixin.java
@@ -1,19 +1,26 @@
 package me.fzzyhmstrs.gear_core.mixins;
 
+import com.google.common.collect.Multimap;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.Trinket;
 import me.fzzyhmstrs.fzzy_core.interfaces.Modifiable;
+import me.fzzyhmstrs.gear_core.interfaces.AttributeTracking;
 import me.fzzyhmstrs.gear_core.interfaces.DamageTracking;
 import me.fzzyhmstrs.gear_core.interfaces.HitTracking;
 import me.fzzyhmstrs.gear_core.interfaces.KillTracking;
 import me.fzzyhmstrs.gear_core.modifier_util.EquipmentModifierHelper;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.UUID;
 
 @Pseudo
 @Mixin(Trinket.class)
@@ -25,7 +32,7 @@ public interface TrinketMixin extends Modifiable, HitTracking, KillTracking, Dam
     
     @ModifyReturnValue(method = "getModifiers", at = @At("RETURN"))
     private Multimap<EntityAttribute, EntityAttributeModifier> gear_core_addModifierModifiersToTrinketModifiers(Multimap<EntityAttribute, EntityAttributeModifier> original, ItemStack stack,
-			SlotReference slot, LivingEntity entity, UUID uuid){
+                                                                                                                SlotReference slot, LivingEntity entity, UUID uuid){
         if (stack.getItem() instanceof AttributeTracking){
             return EquipmentModifierHelper.INSTANCE.getAttributeModifiers(stack, original);
         } else {
