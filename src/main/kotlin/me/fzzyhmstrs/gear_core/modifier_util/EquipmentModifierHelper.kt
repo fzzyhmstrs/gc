e1739a78dd75a141fbd38e3491675284dc366916
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-02-13 20:47:20 -0500

    fixing trinket stuff and removing useless println

--- src/main/kotlin/me/fzzyhmstrs/gear_core/modifier_util/EquipmentModifierHelper.kt
+++ src/main/kotlin/me/fzzyhmstrs/gear_core/modifier_util/EquipmentModifierHelper.kt
@@ -2,7 +2,6 @@ package me.fzzyhmstrs.gear_core.modifier_util
 
 import com.google.common.collect.ArrayListMultimap
 import com.google.common.collect.Multimap
-import dev.emi.trinkets.api.Trinket
 import me.fzzyhmstrs.fzzy_core.coding_util.AcText
 import me.fzzyhmstrs.fzzy_core.modifier_util.AbstractModifier
 import me.fzzyhmstrs.fzzy_core.modifier_util.AbstractModifierHelper
@@ -27,7 +26,6 @@ import net.minecraft.server.world.ServerWorld
 import net.minecraft.text.Text
 import net.minecraft.util.Formatting
 import net.minecraft.util.Identifier
-import net.minecraft.util.registry.Registry
 
 object EquipmentModifierHelper: AbstractModifierHelper<EquipmentModifier>() {
 
@@ -112,6 +110,7 @@ object EquipmentModifierHelper: AbstractModifierHelper<EquipmentModifier>() {
         map.putAll(original)
         val id = Nbt.getItemStackId(stack)
         map.putAll(attributeMap[id]?:EMPTY_ATTRIBUTE_MAP)
+        //println(map)
         return map
     }
 
