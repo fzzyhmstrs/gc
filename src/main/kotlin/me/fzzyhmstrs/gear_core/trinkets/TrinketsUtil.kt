9b8e3e1fdf84aa3d916426e7de141fda56c61105
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-04-07 23:16:42 -0400

    fix how TrinketsUtil.kt interacts with other things that add NBT
    (cherry picked from commit a625a2de8adc33b4aead615fb2f4ec119bd05943)

--- src/main/kotlin/me/fzzyhmstrs/gear_core/trinkets/TrinketsUtil.kt
+++ src/main/kotlin/me/fzzyhmstrs/gear_core/trinkets/TrinketsUtil.kt
@@ -2,12 +2,12 @@ package me.fzzyhmstrs.gear_core.trinkets
 
 import com.google.common.collect.Multimap
 import dev.emi.trinkets.api.Trinket
-import me.fzzyhmstrs.fzzy_core.nbt_util.Nbt
 import net.minecraft.entity.attribute.EntityAttribute
 import net.minecraft.entity.attribute.EntityAttributeModifier
 import net.minecraft.item.ItemStack
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.util.registry.Registry
+import net.minecraft.nbt.NbtList
 
 object TrinketsUtil {
     
@@ -16,16 +16,28 @@ object TrinketsUtil {
     }
 
     fun addTrinketNbt(stack: ItemStack,nbt: NbtCompound, map: Multimap<EntityAttribute, EntityAttributeModifier>){
+
         if (stack.item is Trinket) {
-            nbt.remove("TrinketAttributeModifiers")
+            val existingAttributeList = nbt.getList("TrinketAttributeModifiers",10)
+            val nbtList = NbtList()
+
             for (entry in map.entries()) {
                 val attribute = entry.key
                 val modifier = entry.value
                 val compound = modifier.toNbt()
-                compound.putString("AttributeName", Registry.ATTRIBUTE.getId(attribute).toString())
-                Nbt.addNbtToList(compound, "TrinketAttributeModifiers", nbt)
+                val name = Registry.ATTRIBUTE.getId(attribute).toString()
+                compound.putBoolean("GearCoreModifier",true)
+                compound.putString("AttributeName", name)
+                nbtList.add(compound)
             }
+
+            for (attribute in existingAttributeList){
+                if (attribute !is NbtCompound) continue
+                if (attribute.contains("GearCoreModifier")) continue
+                nbtList.add(attribute)
+            }
+
+            nbt.put("TrinketAttributeModifiers",nbtList)
         }
     }
-
 }
