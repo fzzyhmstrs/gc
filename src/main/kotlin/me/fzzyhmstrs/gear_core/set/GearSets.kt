15f7a7a18fb609090bb8ada22ddf9783196f2e3d
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-08-16 22:00:46 -0400

    start work on implementing a gear set feature (2)
    (cherry picked from commit 0aa0c37943edb90ad54a04ca3598b143923e3194)

--- src/main/kotlin/me/fzzyhmstrs/gear_core/set/GearSets.kt
+++ src/main/kotlin/me/fzzyhmstrs/gear_core/set/GearSets.kt
@@ -1,13 +1,31 @@
 package me.fzzyhmstrs.gear_core.set
 
+import com.google.common.collect.HashMultimap
+import com.google.gson.JsonParser
 import me.fzzyhmstrs.gear_core.GC
 import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener
+import net.minecraft.item.Item
 import net.minecraft.resource.ResourceManager
 import net.minecraft.util.Identifier
 
 object GearSets: SimpleSynchronousResourceReloadListener {
-    override fun reload(manager: ResourceManager?) {
-        TODO("Not yet implemented")
+
+    private var gearSets: Set<GearSet> = setOf()
+    private var cachedSets: HashMultimap<Item,GearSet> = HashMultimap.create()
+
+    override fun reload(manager: ResourceManager) {
+        val gearSets: MutableSet<GearSet> = mutableSetOf()
+        manager.findResources("gear_core/sets") { path -> path.path.endsWith(".json") }
+        .forEach { (id,resource) ->
+            try {
+                val reader = resource.reader
+                val json = JsonParser.parseReader(reader).asJsonObject
+                gearSets.add(GearSet.fromJson(id, json))
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+
     }
 
 
