babfb611d90f5179c6dd6ec74d622ae2305b89bf
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-08-17 09:45:21 -0400

    Create ActiveGearSetsTracking.java

---
+++ src/main/java/me/fzzyhmstrs/gear_core/interfaces/ActiveGearSetsTracking.java
@@ -0,0 +1,11 @@
+package me.fzzyhmstrs.gear_core.interfaces;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+
+public interface ActiveGearSetsTracking {
+    void setActiveSets(Set<GearSet>);
+    Set<GearSet> getActiveSets();
+}
