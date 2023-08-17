7a07cd2d3724548dac895197156cb9b8a7981337
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-08-16 21:36:53 -0400

    start work on implementing a gear set feature

---
+++ src/main/kotlin/me/fzzyhmstrs/gear_core/set/GearSet.kt
@@ -0,0 +1,118 @@
+package me.fzzyhmstrs.gear_core.set
+
+import com.google.common.collect.ArrayListMultimap
+import com.google.common.collect.HashMultimap
+import com.google.common.collect.Multimap
+import com.google.gson.JsonObject
+import com.google.gson.JsonSyntaxException
+import me.fzzyhmstrs.fzzy_core.registry.ModifierRegistry
+import me.fzzyhmstrs.gear_core.modifier_util.EntityAttributeModifierContainer
+import me.fzzyhmstrs.gear_core.modifier_util.EquipmentModifier
+import net.minecraft.entity.attribute.EntityAttribute
+import net.minecraft.recipe.Ingredient
+import net.minecraft.util.Formatting
+import net.minecraft.util.Identifier
+import java.lang.IllegalStateException
+import javax.swing.text.html.HTML.Tag.P
+
+class GearSet private constructor(
+    private val id: Identifier,
+    private val translationKey: String,
+    private val activeFormatting: Array<Formatting>,
+    private val inactiveFormatting: Array<Formatting>,
+    private val items: Ingredient,
+    private val attributeBonuses: Map<Int, Multimap<EntityAttribute,EntityAttributeModifierContainer>>,
+    private val modifierBonuses: Map<Int,List<EquipmentModifier>>) {
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null) return false
+        if (other !is GearSet) return false
+        return other.id == id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
+    companion object{
+        fun fromJson(id: Identifier,json: JsonObject): GearSet{
+            val translationKey = try {
+                json.getAsJsonPrimitive("name").asString
+            } catch (e: Exception){
+                throw IllegalStateException("Gear Set [$id] needs a 'name' member that is a string")
+            }
+            val activeFormattingList: List<Formatting> = if (json.has("active_formatting")) {
+                try {
+                    json.getAsJsonArray("active_formatting")
+                        .map { jsonElement -> jsonElement.asString }
+                        .map { str -> Formatting.byName(str) }
+                        .filterNotNull()
+                } catch (e: Exception) {
+                    throw IllegalStateException("Gear Set [$id] has an 'active_formatting' member that isn't a properly formatted array.")
+                }
+            } else {
+                listOf(Formatting.GOLD)
+            }
+            val inactiveFormattingList: List<Formatting> = if (json.has("inactive_formatting")) {
+                try {
+                    json.getAsJsonArray("inactive_formatting")
+                        .map { jsonElement -> jsonElement.asString }
+                        .map { str -> Formatting.byName(str) }
+                        .filterNotNull()
+                } catch (e: Exception) {
+                    throw IllegalStateException("Gear Set [$id] has an 'inactive_formatting' member that isn't a properly formatted array.")
+                }
+            } else {
+                listOf(Formatting.GOLD)
+            }
+            val items = try {
+                Ingredient.fromJson(json.get("items"))
+            } catch (e: JsonSyntaxException){
+                throw IllegalStateException("Gear Set [$id] has an 'items' member that isn't a properly formatted Ingredient: [${e.localizedMessage}].")
+            }
+            if (!json.has("bonuses") || !json.get("bonuses").isJsonObject)
+                throw IllegalStateException("Gear Set [$id] needs a 'bonuses' member that is a JsonObject")
+            val bonusesJsonObject = json.getAsJsonObject("bonuses")
+            val attributeBonuses: MutableMap<Int, Multimap<EntityAttribute,EntityAttributeModifierContainer>> = mutableMapOf()
+            val modifierBonuses: MutableMap<Int,MutableList<EquipmentModifier>> = mutableMapOf()
+            for (bonus in bonusesJsonObject.entrySet()){
+                val key = try {
+                    val chk = bonus.key.toInt()
+                    if (chk <= 0)
+                        throw java.lang.NumberFormatException()
+                    chk
+                } catch (e: NumberFormatException){
+                    throw java.lang.NumberFormatException("Gear Set [$id] has a bonus with an invalid key [${bonus.key}]. Needs to be a positive integer greater than 0")
+                }
+                val bonusJson = bonus.value
+                if (bonusJson.isJsonPrimitive){
+                    val modifierJson = bonusJson.asString
+                    val modifierId = Identifier.tryParse(modifierJson)
+                        ?: throw IllegalStateException("Gear Set [$id] has a bonus with an invalid identifier value [$modifierJson]. Needs to be a valid identifier string")
+                    val modifier = ModifierRegistry.getByType<EquipmentModifier>(modifierId)
+                        ?: throw IllegalStateException("Gear Set [$id] has a bonus with a modifier value [$modifierJson] that can't be found in the modifier registry.")
+                    modifierBonuses.computeIfAbsent(key) { mutableListOf() }.add(modifier)
+                } else if (bonusJson.isJsonArray){
+                    val bonusesJsonArray = bonusJson.asJsonArray
+                    for (b in bonusesJsonArray){
+                        if (b.isJsonPrimitive){
+                            val modifierJson = bonusJson.asString
+                            val modifierId = Identifier.tryParse(modifierJson)
+                                ?: throw IllegalStateException("Gear Set [$id] has a bonus with an invalid identifier value [$modifierJson]. Needs to be a valid identifier string")
+                            val modifier = ModifierRegistry.getByType<EquipmentModifier>(modifierId)
+                                ?: throw IllegalStateException("Gear Set [$id] has a bonus with a modifier value [$modifierJson] that can't be found in the modifier registry.")
+                            modifierBonuses.computeIfAbsent(key) { mutableListOf() }.add(modifier)
+                        } else if (b.isJsonObject){
+                            val attributeJson = b.asJsonObject
+                            TODO()
+                        }
+                    }
+                } else {
+                    throw IllegalStateException("Gear Set [$id] has an invalid 'bonuses' member. Needs to be a string or an array.")
+                }
+            }
+            return GearSet(id,translationKey,activeFormattingList.toTypedArray(),inactiveFormattingList.toTypedArray(),items,attributeBonuses, modifierBonuses)
+        }
+    }
+
+}
