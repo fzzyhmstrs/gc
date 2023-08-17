e17952bd2805ce21e5f2977d3e16f6e589d8b1fa
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-08-16 21:36:53 -0400

    start work on implementing a gear set feature
    (cherry picked from commit 7a07cd2d3724548dac895197156cb9b8a7981337)

---
+++ src/main/kotlin/me/fzzyhmstrs/gear_core/modifier_util/EntityAttributeModifierContainer.kt
@@ -0,0 +1,24 @@
+package me.fzzyhmstrs.gear_core.modifier_util
+
+import net.minecraft.entity.EquipmentSlot
+import net.minecraft.entity.attribute.EntityAttributeModifier
+import java.util.*
+
+class EntityAttributeModifierContainer(private val name: String, private val amount: Double, private val operation: EntityAttributeModifier.Operation){
+    private val attributes: EnumMap<EquipmentSlot, EntityAttributeModifier> = EnumMap(mapOf(
+        EquipmentSlot.MAINHAND to EntityAttributeModifier(UUID.randomUUID(),name,amount,operation),
+        EquipmentSlot.OFFHAND to EntityAttributeModifier(UUID.randomUUID(),name,amount,operation),
+        EquipmentSlot.HEAD to EntityAttributeModifier(UUID.randomUUID(),name,amount,operation),
+        EquipmentSlot.CHEST to EntityAttributeModifier(UUID.randomUUID(),name,amount,operation),
+        EquipmentSlot.LEGS to EntityAttributeModifier(UUID.randomUUID(),name,amount,operation),
+        EquipmentSlot.FEET to EntityAttributeModifier(UUID.randomUUID(),name,amount,operation)
+    ))
+
+    fun provideEntityAttribute(slot: EquipmentSlot): EntityAttributeModifier {
+        return attributes[slot]?:throw IllegalStateException("slot $slot not found in enum-map for some reason!")
+    }
+
+    fun provideEntityAttributesForTrinkets(): EntityAttributeModifier {
+        return EntityAttributeModifier(UUID.randomUUID(),name,amount,operation)
+    }
+}
