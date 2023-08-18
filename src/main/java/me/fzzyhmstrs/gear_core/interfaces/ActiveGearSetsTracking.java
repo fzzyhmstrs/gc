8879dd95e87560b48936dd00d9654d69af4f502e
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-08-17 21:48:10 -0400

    continue implementing the gear set functionality
    (cherry picked from commit 15a310b711d00a6036f4ffd50f2543482739207a)

--- src/main/java/me/fzzyhmstrs/gear_core/interfaces/ActiveGearSetsTracking.java
+++ src/main/java/me/fzzyhmstrs/gear_core/interfaces/ActiveGearSetsTracking.java
@@ -1,11 +1,10 @@
 package me.fzzyhmstrs.gear_core.interfaces;
 
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.Hand;
-import net.minecraft.world.World;
+import me.fzzyhmstrs.gear_core.set.GearSet;
+
+import java.util.HashMap;
 
 public interface ActiveGearSetsTracking {
-    void setActiveSets(Set<GearSet> sets);
-    Set<GearSet> getActiveSets();
+    void gear_core_setActiveSets(HashMap<GearSet, Integer> sets);
+    HashMap<GearSet, Integer> gear_core_getActiveSets();
 }
--- src/main/kotlin/me/fzzyhmstrs/gear_core/set/GearSets.kt
+++ src/main/kotlin/me/fzzyhmstrs/gear_core/set/GearSets.kt
@@ -1,55 +1,86 @@
 package me.fzzyhmstrs.gear_core.set
 
 import com.google.common.collect.HashMultimap
+import com.google.gson.Gson
 import com.google.gson.JsonParser
+import me.fzzyhmstrs.fzzy_core.trinket_util.TrinketChecker
+import me.fzzyhmstrs.fzzy_core.trinket_util.TrinketUtil
 import me.fzzyhmstrs.gear_core.GC
+import me.fzzyhmstrs.gear_core.interfaces.ActiveGearSetsTracking
+import net.fabricmc.api.EnvType
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper
 import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener
+import net.fabricmc.loader.api.FabricLoader
+import net.minecraft.entity.EquipmentSlot
+import net.minecraft.entity.LivingEntity
 import net.minecraft.item.Item
+import net.minecraft.registry.Registries
 import net.minecraft.resource.ResourceManager
+import net.minecraft.resource.ResourceType
 import net.minecraft.util.Identifier
 
 object GearSets: SimpleSynchronousResourceReloadListener {
 
-    private val gearSets: MutableSet<GearSet> = mutableSetOf()
+    private val gearSets: MutableMap<Identifier,GearSet> = mutableMapOf()
     private val cachedSets: HashMultimap<Item,GearSet> = HashMultimap.create()
-    private val 
+    private val setsToSend: MutableMap<Identifier,String> = mutableMapOf()
 
     private val GEAR_SET_SENDER = Identifier(GC.MOD_ID,"gear_set_sender")
     
     fun registerServer(){
         ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(this)
+        ServerLifecycleEvents.END_DATA_PACK_RELOAD.register{ server,_,_->
+            val maxIndex = setsToSend.size - 1
+            for (player in server.playerManager.playerList){
+                setsToSend.entries.forEachIndexed{ index, entry ->
+                    val buf = PacketByteBufs.create()
+                    buf.writeIdentifier(entry.key)
+                    buf.writeString(entry.value)
+                    buf.writeBoolean(index == maxIndex)
+                    ServerPlayNetworking.send(player, GEAR_SET_SENDER,buf)
+                }
+            }
+        }
     }
 
-    fun registerClient{
-        ClientPlayNetworking.registerGlobalReceiver(GEAR_SET_SENDER) {client,_,buf,_ ->
+    fun registerClient(){
+        ClientPlayNetworking.registerGlobalReceiver(GEAR_SET_SENDER) { _, _, buf, _ ->
+            val id = buf.readIdentifier()
             val jsonString = buf.readString()
-            val json = JsonParser().parse(jsonString).asJsonObject
-            if (buf.readBoolean()){
-                cachedSets.clear()
-                for (item in Registries.ITEM){
-                    for (set in gearSets){
-                        if (set.test(item)){
-                            cachedSets.put(item,set)
+            try {
+                val json = JsonParser.parseString(jsonString).asJsonObject
+                gearSets[id] = (GearSet.fromJson(id, json))
+                if (buf.readBoolean()) {
+                    cachedSets.clear()
+                    for (item in Registries.ITEM) {
+                        for (set in gearSets.values) {
+                            if (set.test(item)) {
+                                cachedSets.put(item, set)
+                            }
                         }
                     }
                 }
+            } catch (e: Exception){
+                e.printStackTrace()
             }
         }
     }
     
     override fun reload(manager: ResourceManager) {
         val gearSets: MutableSet<GearSet> = mutableSetOf()
-        manager.findResources("gear_core/sets") { path -> path.path.endsWith(".json") }
-        .forEach { (id,resource) ->
+        val files = manager.findResources("gear_core/sets") { path -> path.path.endsWith(".json") }
+        val gson = Gson()
+        for (mutableEntry in files.entries) {
             try {
-                val reader = resource.reader
+                val reader = mutableEntry.value.reader
                 val json = JsonParser.parseReader(reader).asJsonObject
-                gearSets.add(GearSet.fromJson(id, json))
+                gearSets.add(GearSet.fromJson(mutableEntry.key, json))
                 if (FabricLoader.getInstance().environmentType == EnvType.SERVER){
-                    val buf = PacketByteBufs.create()
-                    buf.writeString(TODO())
-                    
-                    ServerPlayNetworking.send
+                    setsToSend[mutableEntry.key] = gson.toJson(reader)
                 }
             } catch (e: Exception){
                 e.printStackTrace()
@@ -65,15 +96,17 @@ object GearSets: SimpleSynchronousResourceReloadListener {
         }
     }
 
+
+
     fun updateActiveSets(entity: LivingEntity){
-        val oldActiveMap = (entity as ActiveGearSetTracking).getActiveSets()
-        for (oldSet in oldActiveMap.keySet()){
+        val oldActiveMap = (entity as ActiveGearSetsTracking).gear_core_getActiveSets()
+        for (oldSet in oldActiveMap.keys){
             oldSet.removeAttributesFromEntity(entity)
         }
         val newActiveMap: HashMap<GearSet,Int> = hashMapOf()
-        for (slot in EquipmentSlot.entries){
+        for (slot in EquipmentSlot.values()){
             val stack = entity.getEquippedStack(slot)
-            if (!stack.isEmpty()){
+            if (!stack.isEmpty){
                 val gearSets = cachedSets.get(stack.item)
                 for (gearSet in gearSets){
                     val num = newActiveMap[gearSet] ?: 0
@@ -81,7 +114,7 @@ object GearSets: SimpleSynchronousResourceReloadListener {
                 }
             }
         }
-        if (TrinketChecker.getTrinketsLoaded()) {
+        if (TrinketChecker.trinketsLoaded) {
             val stacks = TrinketUtil.getTrinketStacks(entity)
             for (stack in stacks) {
                 val gearSets = cachedSets.get(stack.item)
@@ -91,10 +124,11 @@ object GearSets: SimpleSynchronousResourceReloadListener {
                 }
             }
         }
-        for (entry in newActiveMap.entrySet()){
+        for (entry in newActiveMap.entries){
             entry.key.addAttributesToEntity(entity,entry.value)
         }
-        (entity as ActiveGearSetTracking).setActiveSets(newActiveMap)
+        (entity as ActiveGearSetsTracking).gear_core_setActiveSets(newActiveMap)
+
     }
 
     override fun getFabricId(): Identifier {
