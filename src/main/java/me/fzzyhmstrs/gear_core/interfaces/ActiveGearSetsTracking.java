6bfb9993f36389c5e63515ae87bc91db42fbbc5a
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-08-18 09:55:50 -0400

    Update ActiveGearSetsTracking.java
    (cherry picked from commit dc9a83176b4774d69b4e2213ab6cd9009b3f2fe4)

--- src/main/java/me/fzzyhmstrs/gear_core/interfaces/ActiveGearSetsTracking.java
+++ src/main/java/me/fzzyhmstrs/gear_core/interfaces/ActiveGearSetsTracking.java
@@ -5,6 +5,8 @@ import me.fzzyhmstrs.gear_core.set.GearSet;
 import java.util.HashMap;
 
 public interface ActiveGearSetsTracking {
+    void markDirty()
+    boolean isDirty()
     void gear_core_setActiveSets(HashMap<GearSet, Integer> sets);
     HashMap<GearSet, Integer> gear_core_getActiveSets();
 }
