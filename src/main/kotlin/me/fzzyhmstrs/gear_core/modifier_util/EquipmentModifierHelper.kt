f87c4bccea23c220e526f7eedadddccdf5fb0623
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-08-24 18:05:41 -0400

    fix targetMapping infinite BLANK modifiers

--- src/main/kotlin/me/fzzyhmstrs/gear_core/modifier_util/EquipmentModifierHelper.kt
+++ src/main/kotlin/me/fzzyhmstrs/gear_core/modifier_util/EquipmentModifierHelper.kt
@@ -228,6 +228,7 @@ object EquipmentModifierHelper: AbstractModifierHelper<EquipmentModifier>() {
     
     internal fun addToTargetMap(modifier: EquipmentModifier){
         if (!modifier.randomlySelectable()) return
+        if (modifier.target == EquipmentModifier.EquipmentModifierTarget.NONE) return
         val target = modifier.target
         val weight = modifier.weight
         for (i in 1..weight){
