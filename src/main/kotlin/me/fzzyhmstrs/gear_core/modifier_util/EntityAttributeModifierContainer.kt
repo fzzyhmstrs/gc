458e8c160af84bf31a04b9302ebd0faebbadf950
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-11-08 10:37:30 -0500

    Update EntityAttributeModifierContainer.kt

--- src/main/kotlin/me/fzzyhmstrs/gear_core/modifier_util/EntityAttributeModifierContainer.kt
+++ src/main/kotlin/me/fzzyhmstrs/gear_core/modifier_util/EntityAttributeModifierContainer.kt
@@ -5,17 +5,10 @@ import net.minecraft.entity.attribute.EntityAttributeModifier
 import java.util.*
 
 class EntityAttributeModifierContainer(private val name: String, private val amount: Double, private val operation: EntityAttributeModifier.Operation, private val uuid: UUID? = null){
-    private val attributes: EnumMap<EquipmentSlot, EntityAttributeModifier> = EnumMap(mapOf(
-        EquipmentSlot.MAINHAND to EntityAttributeModifier(UUID.randomUUID(),name,amount,operation),
-        EquipmentSlot.OFFHAND to EntityAttributeModifier(UUID.randomUUID(),name,amount,operation),
-        EquipmentSlot.HEAD to EntityAttributeModifier(UUID.randomUUID(),name,amount,operation),
-        EquipmentSlot.CHEST to EntityAttributeModifier(UUID.randomUUID(),name,amount,operation),
-        EquipmentSlot.LEGS to EntityAttributeModifier(UUID.randomUUID(),name,amount,operation),
-        EquipmentSlot.FEET to EntityAttributeModifier(UUID.randomUUID(),name,amount,operation)
-    ))
+    private val attributes: MutablMap<SlotId, EntityAttributeModifier> = mutableMapOf()
 
-    fun provideEntityAttribute(slot: EquipmentSlot): EntityAttributeModifier {
-        return attributes[slot]?:throw IllegalStateException("slot $slot not found in enum-map for some reason!")
+    fun provideEntityAttribute(slot: SlotId): EntityAttributeModifier {
+        return attributes.computeIfAbsent(slot) {EntityAttributeModifier(slot.getUUID(name))}
     }
 
     fun provideNonSlotEntityAttribute(): EntityAttributeModifier {
