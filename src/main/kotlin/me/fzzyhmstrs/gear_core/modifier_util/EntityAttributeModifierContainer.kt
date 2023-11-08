4be9037057d4aa951917d017cce6b8ca54ebfe94
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-11-08 11:13:44 -0500

    Update EntityAttributeModifierContainer.kt

--- src/main/kotlin/me/fzzyhmstrs/gear_core/modifier_util/EntityAttributeModifierContainer.kt
+++ src/main/kotlin/me/fzzyhmstrs/gear_core/modifier_util/EntityAttributeModifierContainer.kt
@@ -7,8 +7,8 @@ import java.util.*
 class EntityAttributeModifierContainer(private val name: String, private val amount: Double, private val operation: EntityAttributeModifier.Operation, private val uuid: UUID? = null){
     private val attributes: MutablMap<SlotId, EntityAttributeModifier> = mutableMapOf()
 
-    fun provideEntityAttribute(slot: SlotId): EntityAttributeModifier {
-        return attributes.computeIfAbsent(slot) {EntityAttributeModifier(slot.getUUID(name))}
+    fun provideEntityAttribute(slot: SlotId, offset: Int): EntityAttributeModifier {
+        return attributes.computeIfAbsent(slot) {EntityAttributeModifier(slot.getUUID(name + "#" + offset))}
     }
 
     fun provideNonSlotEntityAttribute(): EntityAttributeModifier {
